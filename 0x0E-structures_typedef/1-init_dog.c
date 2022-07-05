565bf194d844b68ab5e132d3f1d5a890a5bf32eb
GULUMAWAKUMA <gulumawakuma70@gmail.com>
2022-07-05 05:11:50 -0700

    structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a variable of type sturct dog.
+ * @d: Variable to initialize.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+C structure unsing struct
